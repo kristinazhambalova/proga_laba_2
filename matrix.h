dc235acbe084976ad2ed9f74daf5a814656797ea
kristinazhambalova <145702270+kristinazhambalova@users.noreply.github.com>
2024-03-19 13:37:12 +0300

    matrix.h

--- matrix.h
+++ matrix.h
@@ -1,17 +1,17 @@
-﻿#pragma once
-#ifndef MATRIX_H
+#pragma once      //директива обеспечивает защиту от многократного включения заголовочного файла
+#ifndef MATRIX_H  //начинает условную компиляцию, чтобы проверить, был ли файл уже включен ранее
 #define MATRIX_H
 
 #include <iostream>
 
 class Matrix {
 private:
-    int** data;
-    unsigned int m, n;
+    int** data;     //для хранепния данных матрицы
+    unsigned int m, n;   //для хранения количества строк и столбцов матрицы
 
 public:
-    Matrix(unsigned int rows, unsigned int columns); 
-    ~Matrix(); 
+    Matrix(unsigned int rows, unsigned int columns);   //конструктор, создает матрицу заданного размера
+    ~Matrix();                                         //деструктор, освобождает динамически выделенную память
 
     int* operator[](unsigned int i); 
 
